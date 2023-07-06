490d64451d3cbec3a9ad3c45f0a0c7a0b23c0cfb
eddie-an <edward.an@ucalgary.ca>
2023-07-06 00:20:16 -0600

    superficial changes

--- src/components/CompoundingInterest.js
+++ src/components/CompoundingInterest.js
@@ -6,7 +6,6 @@ function CompoundingInterest({ RadioButton }) {
     const [ principal, setPrincipal ] = useState(0);
     const [ futureValue, setFutureValue ] = useState(0);
     const [ compoundingPeriod, setCompoundingPeriod] = useState("");
-    const [ showEarEpr, setShowEarEpr ] = useState(false);
     const [ answer, setAnswer ] = useState(0);
     const [ mode, setMode ] = useState("futureValue");
 
