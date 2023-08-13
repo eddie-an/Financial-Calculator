3672dd5c1475943c3deb8c4a64891b8538e98b66
Edward <edward.an@ucalgary.ca>
2023-08-13 03:28:54 -0600

    major improvements to mobile support

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,15 +1,51 @@
 import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+
+
+function OverLay({setPopupState}) {
+    return (
+        <div className="overlay-container">
+            <MenuComponents/>
+            <div className='close-button-container'> 
+                <button onClick={ () => setPopupState(false) }>X</button>
+            </div>
+
+        </div>
+    );
+}
 
 function NavigationBar() {
+
+    const [ popupState, setPopupState] = useState(false); 
+
     return(
+    popupState === true ?
+    <OverLay setPopupState={setPopupState}/> :
     <div className="navigation-bar">
+        <div className="navigation-hamburger-menu-container">
+            <button className="hamburger-menu-button" onClick={()=>setPopupState(true)}>&#9776;</button>
+        </div>
         <div className="navigation-title-container">
             <h1>Financial Calculator</h1>
         </div>
+        <div className="navigation-menu-components">
+            <MenuComponents/>
+        </div>
+    </div>  
+    
+    
+    );
+}
+
+
+
+function MenuComponents() {
+    return (
+    <>
         <NavLink to="/simple-interest" >
-            <div className="navigation-button-container">
-                <p>Simple Interest</p>
-            </div>
+        <div className="navigation-button-container">
+            <p>Simple Interest</p>
+        </div>
         </NavLink>
         <NavLink to="/compounding-interest">
             <div className="navigation-button-container">
@@ -21,8 +57,8 @@ function NavigationBar() {
                 <p>Uniform Series Cash Flow</p>
             </div>
         </NavLink>
-    </div>  
-    );
+    </>
+    )
 }
 
 export default NavigationBar;
