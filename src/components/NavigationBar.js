266b84ec023e763ffc92574f20949b6def6bdab7
Edward <edward.an@ucalgary.ca>
2023-08-18 00:07:31 -0600

    styled the overlay menu buttons

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,7 +5,21 @@ import React, { useState } from "react";
 function OverLay({setPopupState}) {
     return (
         <div className="overlay-container">
-            <MenuComponents/>
+            <NavLink to="/simple-interest" onClick={ () => setPopupState(false)}>
+            <div className="overlay-button-container">
+                <p>Simple Interest</p>
+            </div>
+            </NavLink>
+            <NavLink to="/compounding-interest">
+                <div className="overlay-button-container" onClick={ () => setPopupState(false)}>
+                    <p>Compounding Interest</p>
+                </div>
+            </NavLink>
+            <NavLink to="/uniform-series-cash-flow">
+                <div className="overlay-button-container" onClick={ () => setPopupState(false)}>
+                    <p>Uniform Series Cash Flow</p>
+                </div>
+            </NavLink>
             <div className='close-button-container'> 
                 <button onClick={ () => setPopupState(false) }>X</button>
             </div>
@@ -29,7 +43,21 @@ function NavigationBar() {
             <h1>Financial Calculator</h1>
         </div>
         <div className="navigation-menu-components">
-            <MenuComponents/>
+            <NavLink to="/simple-interest" >
+            <div className="navigation-button-container">
+                <p>Simple Interest</p>
+            </div>
+            </NavLink>
+            <NavLink to="/compounding-interest">
+                <div className="navigation-button-container">
+                    <p>Compounding Interest</p>
+                </div>
+            </NavLink>
+            <NavLink to="/uniform-series-cash-flow">
+                <div className="navigation-button-container">
+                    <p>Uniform Series Cash Flow</p>
+                </div>
+            </NavLink>
         </div>
     </div>  
     
@@ -38,27 +66,5 @@ function NavigationBar() {
 }
 
 
-
-function MenuComponents() {
-    return (
-    <>
-        <NavLink to="/simple-interest" >
-        <div className="navigation-button-container">
-            <p>Simple Interest</p>
-        </div>
-        </NavLink>
-        <NavLink to="/compounding-interest">
-            <div className="navigation-button-container">
-                <p>Compounding Interest</p>
-            </div>
-        </NavLink>
-        <NavLink to="/uniform-series-cash-flow">
-            <div className="navigation-button-container">
-                <p>Uniform Series Cash Flow</p>
-            </div>
-        </NavLink>
-    </>
-    )
-}
 
 export default NavigationBar;
